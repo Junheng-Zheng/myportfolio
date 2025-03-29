a191c5930bc5fb94121c259eabd25b0427383d71
Junheng Zheng <junhengzheng@Junhengs-MacBook-Air.local>
2025-03-29 00:15:39 -0400

    main, fixing vercel issue

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,18 +2,9 @@ const config = {
   plugins: ["@tailwindcss/postcss"],
 };
 
-module.exports = {
-  theme: {
-    extend: {
-      fontSize: {
-        dynamic: "clamp(1.5rem, 5vw, 3rem)", // Dynamic font size using clamp
-      },
-    },
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  plugins: [
-    // If you need to use any Tailwind plugins, you can add them here, for example:
-    // require('@tailwindcss/forms'),
-  ],
 };
-
-export default config;
